bbfc58efc23bf7663c28cbd225e0731d1696531b
liusen <liusen@allinmd.cn>
2017-11-14 11:48:09 +0800

    fix(fastdfs)

--- FastDFS/code/learn-FastDFS/src/main/java/com/lsieun/fastdfs/TestFastDFS.java
+++ FastDFS/code/learn-FastDFS/src/main/java/com/lsieun/fastdfs/TestFastDFS.java
@@ -8,18 +8,24 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.UUID;
 
 public class TestFastDFS {
-    public String conf_filename = "E:\\workspace-maven\\learn-FastDFS\\src\\main\\resources\\fdfs_client.conf";
-    public String local_filename = "E:\\workspace-maven\\learn-FastDFS\\src\\main\\resources\\logo.jpg";
+    //public String conf_filename = "E:\\workspace-maven\\learn-FastDFS\\src\\main\\resources\\fdfs_client.conf";
+    //public String local_filename = "E:\\workspace-maven\\learn-FastDFS\\src\\main\\resources\\logo.jpg";
+    private String conf_filename;
+    private String local_filename;
+    private String classpath;
 
     @Before
     public void setUp() throws Exception {
-
+        classpath = new File(TestFastDFS.class.getResource("/").getFile()).getCanonicalPath();
+        conf_filename = classpath + File.separator + "fdfs_client.conf";
+        local_filename = classpath + File.separator + "logo.jpg";
     }
 
     @After
